5dea689a5bfe89ad641c874c4f67e1c62e320fc7
残照影 <89765291+afterglowsdev@users.noreply.github.com>
2026-03-18 00:36:15 +0800

    Update files

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        // Android Gradle Plugin lives in Google's Maven repository.
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WistTrans"
+
